53233e9286e9cf29db9746aa9877b583a9afa13f
Reza Ageng <rezaageng5@gmail.com>
2022-01-28 19:40:22 +0700

    add search but still error lol

--- src/pages/Anime.js
+++ src/pages/Anime.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom"
 export default function Anime() {
   const [anime, setAnime] = useState([])
   const [loading, setLoading] = useState(true)
+  const [search, setSearch] = useState(false)
+  const [keyword, setKeyword] = useState("")
 
   useEffect(() => {
     async function getAnime() {
@@ -14,30 +16,49 @@ export default function Anime() {
       setLoading(false)
     }
 
-    getAnime()
-  }, [])
+    async function searchAnime(keyword) {
+      const request = await fetch(`https://api.jikan.moe/v4/anime?q=${keyword}`)
+      const response = await request.json()
+
+      setAnime(response.data)
+      setLoading(false)
+    }
+
+    search ? searchAnime() : getAnime()
+  }, [keyword, search])
 
   return (
-    <section>
-      {loading ? (
-        <p>Loading.....</p>
-      ) : (
-        <ul>
-          {anime.map((anm) => (
-            <li key={anm.mal_id}>
-              <img
-                className="list-cover"
-                src={anm.images.jpg.image_url}
-                alt="anime cover"
-              />
-              <h3 className="list-title">
-                <Link to={`/anime/${anm.mal_id}`}>{anm.title}</Link>
-              </h3>
-              <span className="list-score">{anm.score}</span>
-            </li>
-          ))}
-        </ul>
-      )}
-    </section>
+    <>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          placeholder="Search anime..."
+          value={keyword}
+          onChange={(event) => setKeyword(event.target.value)}
+        />
+        <button>Search</button>
+      </form>
+      <section>
+        {loading ? (
+          <p>Loading.....</p>
+        ) : (
+          <ul>
+            {anime.map((anm) => (
+              <li key={anm.mal_id}>
+                <img
+                  className="list-cover"
+                  src={anm.images.jpg.image_url}
+                  alt="anime cover"
+                />
+                <h3 className="list-title">
+                  <Link to={`/anime/${anm.mal_id}`}>{anm.title}</Link>
+                </h3>
+                <span className="list-score">{anm.score}</span>
+              </li>
+            ))}
+          </ul>
+        )}
+      </section>
+    </>
   )
 }
