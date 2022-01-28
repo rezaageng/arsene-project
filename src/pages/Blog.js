425507ec20242092f73314d4212624a7004dc0c2
Reza Ageng <rezaageng5@gmail.com>
2022-01-28 14:20:45 +0700

    fetch api

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,3 +1,33 @@
+import { useEffect, useState } from "react"
+
 export default function Blog() {
-  return <h1>Blog</h1>
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function getArticles() {
+      const request = await fetch("https://animechan.vercel.app/api/quotes")
+      const response = await request.json()
+
+      setArticles(response)
+      setLoading(false)
+      console.log(response)
+    }
+
+    getArticles()
+  }, [])
+
+  return (
+    <section>
+      {loading ? (
+        <p>Loading.....</p>
+      ) : (
+        <div>
+          {articles.map((article) => (
+            <article key={article.quote}>{article.anime}</article>
+          ))}
+        </div>
+      )}
+    </section>
+  )
 }
